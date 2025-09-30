d3f4f03e4f50c47d0659a20140523ee7b234f096
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-10-01 04:16:30 +0500

    Add Domain Events

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -2,9 +2,25 @@
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> domainEvents = new();
     protected Entity(Guid id)
     {
         Id = id;
     }
     public Guid Id { get; init; }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        domainEvents.Add(domainEvent);
+    }
 }
