28553ead00d192a2e7c43b0e553d6e3a039f8795
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-10-01 04:55:00 +0500

    add Booking

---
+++ src/Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,45 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public sealed class Booking : Entity
+{
+    public Booking(
+        Guid id,
+        Guid appartmentId,
+        Guid userId,
+        DateRange duration,
+        Money priceForPeriod,
+        Money cleaningFee,
+        Money amentiesUpCharge,
+        Money totalPrice,
+        BookingStatus status,
+        DateTime createdOnUtc) 
+        : base(id)
+    {
+        ApartmentId = appartmentId;
+        UserId = userId;
+        Duration = duration;
+        PriceForPeriod = priceForPeriod;
+        CleaningFee = cleaningFee;
+        AmenitiesUpCharge = amentiesUpCharge;
+        TotalPrice = totalPrice;
+        Status = status;
+        CreatedOnUtc = createdOnUtc;
+    }
+
+    public Guid ApartmentId { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateRange Duration { get; private set; }
+    public Money PriceForPeriod { get; private set; }
+    public Money CleaningFee { get; private set; }
+    public Money AmenitiesUpCharge { get; private set; }
+    public Money TotalPrice { get; private set; }
+    public BookingStatus Status { get; private set; }
+    public DateTime CreatedOnUtc { get; private set; }
+    public DateTime? ConfirmedOnUtc { get; private set; }
+    public DateTime? RejectedOnUtc { get; private set; }
+    public DateTime? CancelledOnUtc { get; private set; }
+    public DateTime? CompletedOnUtc { get; private set; }
+}
---
+++ src/Bookify.Domain/Bookings/DateRange.cs
@@ -0,0 +1,22 @@
+﻿namespace Bookify.Domain.Bookings;
+
+public record DateRange
+{
+    private DateRange() { }
+    public DateOnly Start { get; init; }
+    public DateOnly End { get; init; }
+    public int LenghtInDays => (End.DayNumber - Start.DayNumber);
+
+    public static DateRange Create(DateOnly start, DateOnly end)
+    {
+        if (end <= start)
+        {
+            throw new ApplicationException("End date precedes start date.");
+        }
+        return new DateRange
+        {
+            Start = start,
+            End = end
+        };
+    }
+}
