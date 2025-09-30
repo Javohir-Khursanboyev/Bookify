8169eda76eed557a7ad384bbd07846500fe36a0f
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-10-01 04:22:18 +0500

    Add Repository interface

---
+++ src/Bookify.Domain/Apartments/IApartmentRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public interface IApartmentRepository
+{
+    Task<Apartment?> SelectByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    void Insert(Apartment apartment);
+}
