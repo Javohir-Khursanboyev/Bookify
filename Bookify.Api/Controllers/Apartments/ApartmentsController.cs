3803d887da031ae6905d616b92ea36adad427ddc
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-11-01 04:13:06 +0500

    add api

---
+++ Bookify.Api/Controllers/Apartments/ApartmentsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Apartments;
+
+[ApiController]
+[Route("api/apartments")]
+public class ApartmentsController : ControllerBase
+{
+
+}
